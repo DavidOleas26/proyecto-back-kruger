1764159662235e5942aab6bac31899150a5e359d
David <52804242+DavidOleas26@users.noreply.github.com>
2025-04-26 12:20:49 -0500

    endpoints de Flat, getAllfltas, byid, update, create, delete

--- controllers/flat.controller.js
+++ controllers/flat.controller.js
@@ -1,16 +1,86 @@
-import { Flat } from "../models/flat.model.js";
+import { validateFlatSchema, validateUpdateFlatSchema } from "../schemas/flat.schema.js";
+import { FlatService } from "../services/flat.service.js";
 
-const addFlat = (req, res) => {
-  try {
-    const body = req.body
-    console.log(body)
-  } catch (error) {
-    res.status(500).json({message: error.message})
+export class FlatController {
+
+  static getAllFlats = async (req, res) => {
+    try {
+      const flats = await FlatService.getAllFlats()
+      res.status(201).json(flats)
+    } catch (error) {
+      res.status(500).json({message: error.message})
+    }
   }
+
+  static getFlatById = async (req, res) => {
+    try {
+      const { id } = req.params
+      const flat = await FlatService.getFlatById(id)
+
+      if (!flat) {
+        return res.status(404).send({ message: "Flat not found" })
+      }
+
+      res.status(200).send(flat);
+
+    } catch (error) {
+      res.status(500).json({message: error.message})
+    }
+  }
+
+  static addFlat = async(req, res) => {
+    try {
+      const { error, value } = validateFlatSchema.validate(req.body)
+  
+      if (error) {
+        return res.status(400).json({message: error.details[0].message})
+      }
+  
+      const flat = await FlatService.saveFlat(req.body)
+      res.status(201).json(flat)
+      
+    } catch (error) {
+      res.status(500).json({message: error.message})
+    }
+  }
+
+  static updateFlat = async(req, res) => {
+    try {
+      const { error, value } = validateUpdateFlatSchema.validate(req.body)
+  
+      if (error) {
+        return res.status(400).json({message: error.details[0].message})
+      }
+  
+      const { id } = req.params
+      const flat = await FlatService.updateFlat(id, req.body)
+  
+      if ( !flat ) {
+        return res.status(404).json({message: "Flat not found"})
+      }
+  
+      res.status(201).json(flat)
+  
+    } catch (error) {
+      res.status(500).json({message: error.message})
+    }
+  }
+
+  static deleteFlat = async (req, res) => {
+    try {
+      const { id } = req.params
+      const flat = await FlatService.deleteFlat(id)
+      
+      if ( !flat ) {
+        return res.status(404).json({message: "Flat not found"})
+      }
+  
+      res.status(201).json(flat)
+
+    } catch (error) {
+      res.status(500).json({message: error.message})
+    }
+  }
+
 }
 
-const getAllFlats = () => {
-
-}
-
-export { getAllFlats, addFlat }
--- schemas/flat.schema.js
+++ schemas/flat.schema.js
@@ -1,16 +1,33 @@
 import Joi from 'joi'
 
+const prsentYear = new Date().getFullYear()
+const maxYearsOld = 100
+const minYear = new Date(prsentYear - maxYearsOld, 0, 1)
+
 const validateFlatSchema = Joi.object(
   {
-    city: Joi.string().required(),
-    streetName: Joi.string().required(),
-    streetNumber: Joi.number().required(),
-    areaSize: Joi.number(),
-    hasAc: Joi.boolean(),
-    yearBuilt: Joi.number(),
-    rentPrice: Joi.number(),
-    dateAvailable: Joi.number()
+    city: Joi.string().required().max(50),
+    streetName: Joi.string().required().max(50),
+    streetNumber: Joi.number().required().integer().positive(),
+    areaSize: Joi.number().required().positive(),
+    hasAc: Joi.boolean().required(),
+    yearBuilt: Joi.date().required().greater(minYear),
+    rentPrice: Joi.number().required().precision(2).positive(),
+    dateAvailable: Joi.date().required().min('now'),
   }
 )
 
-export { validateFlatSchema }
+const validateUpdateFlatSchema = Joi.object(
+  {
+    city: Joi.string().max(50),
+    streetName: Joi.string().max(50),
+    streetNumber: Joi.number().integer().positive(),
+    areaSize: Joi.number().positive(),
+    hasAc: Joi.boolean(),
+    yearBuilt: Joi.date().greater(minYear),
+    rentPrice: Joi.number().precision(2).positive(),
+    dateAvailable: Joi.date().min('now'),
+  }
+)
+
+export { validateFlatSchema, validateUpdateFlatSchema }
