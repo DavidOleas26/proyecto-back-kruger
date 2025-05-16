571bcde0d87cc846f47e6dda9464fcbf748baa2f
David <52804242+DavidOleas26@users.noreply.github.com>
2025-05-16 11:51:59 -0500

    favorite flats router, controller, service, model

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./db/db.js";
 import flatRouter from "./routes/flat.router.js";
 import userRouter from "./routes/user.router.js"
 import authRouter from "./routes/auth.router.js";
+import favoriteFlatsRouter from "./routes/favoriteFlats.router.js";
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,7 @@ connectDB();
 app.use("/flats", flatRouter);
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
+app.use("/favorite_flats", favoriteFlatsRouter)
 
 const PORT = process.env.PORT ?? 8080;
 
