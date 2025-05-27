0f0fc7d204add547f93a5ef5e37f97678235f029
David <52804242+DavidOleas26@users.noreply.github.com>
2025-05-27 18:26:40 -0500

    aumentar cors en server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { connectDB } from "./db/db.js";
+import cors from 'cors'
 
 import flatRouter from "./routes/flat.router.js";
 import userRouter from "./routes/user.router.js"
@@ -8,6 +9,7 @@ import favoriteFlatsRouter from "./routes/favoriteFlats.router.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.disable("x-powered-by");
 
 connectDB();
